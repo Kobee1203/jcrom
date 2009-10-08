7c213f60e7520b3017ebb6b5591d52e847f0a7c8
oli.gauti <oli.gauti@cbd97fa9-d044-0410-a9ef-4d2f922b82d7>
2009-10-08 19:20:35 +0000

    Issue #82 - Added a unit test to test the issue reported, seems fine

--- jcrom/src/test/java/org/jcrom/VersionedEntity.java
+++ jcrom/src/test/java/org/jcrom/VersionedEntity.java
@@ -7,9 +7,6 @@ import org.jcrom.annotations.JcrBaseVersionCreated;
 import org.jcrom.annotations.JcrBaseVersionName;
 import org.jcrom.annotations.JcrCheckedout;
 import org.jcrom.annotations.JcrChildNode;
-import org.jcrom.annotations.JcrName;
-import org.jcrom.annotations.JcrNode;
-import org.jcrom.annotations.JcrPath;
 import org.jcrom.annotations.JcrProperty;
 import org.jcrom.annotations.JcrVersionCreated;
 import org.jcrom.annotations.JcrVersionName;
@@ -18,108 +15,96 @@ import org.jcrom.annotations.JcrVersionName;
  *
  * @author Olafur Gauti Gudmundsson
  */
-@JcrNode(
-	mixinTypes= {"mix:versionable"}
-)
-public class VersionedEntity {
+public class VersionedEntity extends AbstractJcrEntity implements VersionedInterface {
 
-	@JcrPath private String path;
-	@JcrName private String name;
-	@JcrProperty private String title;
-	@JcrProperty private String body;
-	
-	@JcrBaseVersionName private String baseVersionName;
-	@JcrBaseVersionCreated private Date baseVersionCreated;
-	@JcrVersionName private String versionName;
-	@JcrVersionCreated private Date versionCreated;
-	
-	@JcrCheckedout private boolean checkedOut;
-    
-    @JcrChildNode(containerMixinTypes={"mix:versionable"}) List<VersionedEntity> versionedChildren;
-    @JcrChildNode List<Child> unversionedChildren;
+    @JcrProperty
+    private String title;
+    @JcrProperty
+    private String body;
+    @JcrBaseVersionName
+    private String baseVersionName;
+    @JcrBaseVersionCreated
+    private Date baseVersionCreated;
+    @JcrVersionName
+    private String versionName;
+    @JcrVersionCreated
+    private Date versionCreated;
+    @JcrCheckedout
+    private boolean checkedOut;
+    @JcrChildNode(containerMixinTypes = {"mix:versionable"})
+    List<VersionedEntity> versionedChildren;
+    @JcrChildNode
+    List<Child> unversionedChildren;
+    @JcrChildNode(createContainerNode = true, containerMixinTypes = {"mix:versionable"})
+    VersionedEntity versionedChild1;
+    @JcrChildNode(createContainerNode = true)
+    VersionedEntity versionedChild2;
+    @JcrChildNode(createContainerNode = false, containerMixinTypes = {"mix:versionable"})
+    VersionedEntity versionedChild3;
+    @JcrChildNode(createContainerNode = false)
+    VersionedEntity versionedChild4;
 
-    @JcrChildNode(createContainerNode=true,  containerMixinTypes={"mix:versionable"}) VersionedEntity versionedChild1;
-    @JcrChildNode(createContainerNode=true                                          ) VersionedEntity versionedChild2;
-    @JcrChildNode(createContainerNode=false, containerMixinTypes={"mix:versionable"}) VersionedEntity versionedChild3;
-    @JcrChildNode(createContainerNode=false                                         ) VersionedEntity versionedChild4;
-    
     public VersionedEntity() {
         this.versionedChildren = new ArrayList<VersionedEntity>();
         this.unversionedChildren = new ArrayList<Child>();
     }
 
-	public Date getBaseVersionCreated() {
-		return baseVersionCreated;
-	}
+    public Date getBaseVersionCreated() {
+        return baseVersionCreated;
+    }
 
-	public void setBaseVersionCreated(Date baseVersionCreated) {
-		this.baseVersionCreated = baseVersionCreated;
-	}
+    public void setBaseVersionCreated(Date baseVersionCreated) {
+        this.baseVersionCreated = baseVersionCreated;
+    }
 
-	public String getBaseVersionName() {
-		return baseVersionName;
-	}
+    public String getBaseVersionName() {
+        return baseVersionName;
+    }
 
-	public void setBaseVersionName(String baseVersionName) {
-		this.baseVersionName = baseVersionName;
-	}
+    public void setBaseVersionName(String baseVersionName) {
+        this.baseVersionName = baseVersionName;
+    }
 
-	public boolean isCheckedOut() {
-		return checkedOut;
-	}
+    public boolean isCheckedOut() {
+        return checkedOut;
+    }
 
-	public void setCheckedOut(boolean checkedOut) {
-		this.checkedOut = checkedOut;
-	}
+    public void setCheckedOut(boolean checkedOut) {
+        this.checkedOut = checkedOut;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+        this.name = title;
+    }
 
-	public String getPath() {
-		return path;
-	}
+    public Date getVersionCreated() {
+        return versionCreated;
+    }
 
-	public void setPath(String path) {
-		this.path = path;
-	}
+    public void setVersionCreated(Date versionCreated) {
+        this.versionCreated = versionCreated;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getVersionName() {
+        return versionName;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-		this.name = title;
-	}
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
 
-	public Date getVersionCreated() {
-		return versionCreated;
-	}
+    public String getBody() {
+        return body;
+    }
 
-	public void setVersionCreated(Date versionCreated) {
-		this.versionCreated = versionCreated;
-	}
-
-	public String getVersionName() {
-		return versionName;
-	}
-
-	public void setVersionName(String versionName) {
-		this.versionName = versionName;
-	}
-
-	public String getBody() {
-		return body;
-	}
-
-	public void setBody(String body) {
-		this.body = body;
-	}
+    public void setBody(String body) {
+        this.body = body;
+    }
 
     public List<Child> getUnversionedChildren() {
         return unversionedChildren;
@@ -128,8 +113,8 @@ public class VersionedEntity {
     public void setUnversionedChildren(List<Child> unversionedChildren) {
         this.unversionedChildren = unversionedChildren;
     }
-    
-    public void addUnversionedChild( Child child ) {
+
+    public void addUnversionedChild(Child child) {
         unversionedChildren.add(child);
     }
 
@@ -140,9 +125,8 @@ public class VersionedEntity {
     public void setVersionedChildren(List<VersionedEntity> versionedChildren) {
         this.versionedChildren = versionedChildren;
     }
-    
-    public void addVersionedChild( VersionedEntity child ) {
+
+    public void addVersionedChild(VersionedEntity child) {
         versionedChildren.add(child);
     }
-	
 }
